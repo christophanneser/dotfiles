fd5e865c6a71eeb5abc947620107291d81916654
Christoph Anneser <anneser@in.tum.de>
2020-08-31 17:04:29 +0200

    update

---
+++ prettify_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import json;
+import sys
+
+f = open(sys.argv[1], 'r')
+parsed = json.load(f)
+print(parsed)
+f.close()
+
+with open(sys.argv[1], 'w') as outfile:
+    out = json.dumps(parsed, sort_keys=True, indent=4, ensure_ascii=False)
+    outfile.write(out)

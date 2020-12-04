169e78f19a241ef21bb6a45d8e371754eecd8832
Christoph Anneser <anneser@in.tum.de>
2020-12-04 15:04:51 +0100

    update json formatter

--- utils/prettify_json.py
+++ utils/prettify_json.py
@@ -4,9 +4,14 @@ import json
 import sys
 
 f = open(sys.argv[1], 'r')
-parsed = json.load(f)
-print(parsed)
-f.close()
+try:
+    parsed = json.load(f)
+    f.close()
+except Exception as e:
+    print(e)
+    print("json not properly formatted!")
+    exit(1)
+print("json valid!")
 
 with open(sys.argv[1], 'w') as outfile:
     out = json.dumps(parsed, sort_keys=True, indent=4, ensure_ascii=False)

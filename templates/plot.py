853166e4e1a7bcac0c2b55d009024ae6ad83b3d1
Christoph Anneser <anneser@in.tum.de>
2021-05-21 14:08:04 +0200

    format template file

--- templates/plot.py
+++ templates/plot.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib import rc
-import seaborn as sns
+import numpy as np
 import os
+import pandas as pd
+import seaborn as sns
 
 rc('text', usetex=True)
 
@@ -14,9 +14,9 @@ CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a', '#f781bf',
 
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=CB_color_cycle)
 
-DATAFOLDER = "data/"
+DATA_FOLDER = "data/"
 OUTPUT_FOLDER = "plots/"
-FILENAME = "plot.pdf"
+FILE_NAME = "plot.pdf"
 
 FIG_ROWS = 1
 FIG_COLS = 1
@@ -29,5 +29,7 @@ h1, = axes.plot(range(0, 10), range(0, 10), label="linear function")
 handles = [h1, ]
 legend = fig.legend(handles=handles)
 
-fig.savefig(OUTPUT_FOLDER + FILENAME, bbox_extra_artists=(legend,), bbox_inches='tight')
-os.system("pdfcrop {0} {0}".format(OUTPUT_FOLDER + FILENAME))  # crop blank space at margins
+fig.savefig(OUTPUT_FOLDER + FILE_NAME,
+            bbox_extra_artists=(legend,), bbox_inches='tight')
+# crop blank space at margins
+os.system("pdfcrop {0} {0}".format(OUTPUT_FOLDER + FILE_NAME))
